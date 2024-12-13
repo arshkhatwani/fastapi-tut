7ee36371e23a2025f09cadc21c3e914237ef16d4
arshkhatwani <arsh.khatwani@gmail.com>
2024-12-14 01:24:15 +0530

    adds response model

--- router/users.py
+++ router/users.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends
-from schemas import UserBase
+from schemas import UserBase, UserDisplay
 from sqlalchemy.orm import Session
 from db.database import get_db
 from db.db_user import create_user
@@ -8,7 +8,7 @@ from db.db_user import create_user
 router = APIRouter(prefix='/users', tags=['users'])
 
 
-@router.post('/')
+@router.post('/', response_model=UserDisplay)
 def add_user(request: UserBase, db_session: Session = Depends(get_db)):
     user = create_user(db_session, request)
     return user
--- schemas.py
+++ schemas.py
@@ -5,3 +5,12 @@ class UserBase(BaseModel):
     name: str
     email: str
     password: str
+
+
+class UserDisplay(BaseModel):
+    name: str
+    email: str
+
+    # Working without this also
+    class Config:
+        orm_mode = True
