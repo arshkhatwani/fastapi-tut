ecc4249f1e3e9434d4380e5db79163bc1995750b
arshkhatwani <arsh.khatwani@gmail.com>
2024-12-13 20:48:22 +0530

    adds sqlalchemy

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI
 from router import blogs
+from db import models
+from db.database import engine
+
 
 app = FastAPI()
 
 app.include_router(blogs.router)
 
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+models.Base.metadata.create_all(engine)
